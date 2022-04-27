f9d8e622027cef516c76b2fb792fca579f9d0488
Janaina <janainag.teixeira@outlook.com>
2022-04-27 08:51:27 -0300

    :sparkles: feat: get id usuário

--- src/main/java/org/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blogpessoal/controller/UsuarioController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,6 +35,16 @@ public class UsuarioController {
 	@GetMapping("/all")
 	public ResponseEntity<List<Usuario>> getAll() {
 		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+	
+	@GetMapping ("/{id}")
+	public ResponseEntity<Usuario> getById (@PathVariable Long id){
+		return usuarioRepository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping ("/nome/{nome}")
+	public ResponseEntity<List<Usuario>> getByName (@PathVariable String nome){
+		return ResponseEntity.ok(usuarioRepository.findAllByNomeContainingIgnoreCase(nome));	
 	}
 
 	@PostMapping("/cadastrar")
